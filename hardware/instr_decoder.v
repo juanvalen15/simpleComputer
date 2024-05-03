8f3064495cb75ce045dcc48f6865b3544504bbac
Juan <juanvalen15@gmail.com>
2024-05-03 16:50:48 +0200

    moved architecture diagrams to basic folder. VHDL in progress

--- hardware/instr_decoder.v
+++ hardware/instr_decoder.v
@@ -1,49 +1,72 @@
 module instr_decoder
 (
-	input      [2:0] opcode,
+	input      		[3:0]	opcode,
+	input signed	[31:0] 	acc,
 
-	output reg [1:0] ula_op,
-	output reg       pc_en,
-	output reg       mem_wr
+	output reg 		[2:0] 	ula_op,
+	output reg      		pc_load,
+	output reg       		mem_wr
 );
+
+wire nzero = acc != 0;
 
 always @ (*) begin
 	case (opcode)
-		3'b000:  begin                     // NOP
-						ula_op <= 2'd0;
-						pc_en  <= 1'b1;
-						mem_wr <= 1'b0;
-					end
-		3'b001:  begin                     // STOP
-						ula_op <= 2'd0;
+		// NOP: stops processing
+		4'b0000:  begin                     
+						ula_op <= 3'd0;
 						pc_en  <= 1'b0;
 						mem_wr <= 1'b0;
 					end
-		3'b010:  begin                     // LOAD
-						ula_op <= 2'd1;
-						pc_en  <= 1'b1;
+		// LOAD: data from memory into the accumulator register
+		4'b0001:  begin                     
+						ula_op <= 3'd1;
+						pc_en  <= 1'b0;
 						mem_wr <= 1'b0;
-					end
-		3'b011:  begin                     // SET
-						ula_op <= 2'd0;
-						pc_en  <= 1'b1;
+					end					
+		// SET: loads accumulator register value into memory
+		4'b0010:  begin                     
+						ula_op <= 3'd0;
+						pc_en  <= 1'b0;
 						mem_wr <= 1'b1;
-					end
-		3'b100:  begin                     // ADD
-						ula_op <= 2'd2;
-						pc_en  <= 1'b1;
-						mem_wr <= 1'b0;
-					end
-		3'b101:  begin                     // MULT
-						ula_op <= 2'd3;
-						pc_en  <= 1'b1;
-						mem_wr <= 1'b0;
-					end
-		default: begin
-						ula_op <= 2'd0;
+					end				
+		// ADD: adds memory data to the accumulator register
+		4'b0011:  begin                     
+						ula_op <= 3'd2;
+						pc_en  <= 1'b0;
+						mem_wr <= 1'b1;
+					end										
+		// MULT: multiply memory data with data from the accumulator register
+		4'b0100:  begin                     
+						ula_op <= 3'd3;
 						pc_en  <= 1'b0;
 						mem_wr <= 1'b0;
+					end					
+		// JNZ: jump to a new address if accumulator register is not zero
+		4'b0101:  begin                     
+						ula_op <= 3'd0;
+						pc_en  <= nzero;
+						mem_wr <= 1'b0;
+					end				
+		// JZ: jump to a new address if accumulator register is zero
+		4'b0110:  begin                     
+						ula_op <= 3'd0;
+						pc_en  <= !nzero;
+						mem_wr <= 1'b0;
+					end		
+		// JMP: jump to a new address
+		4'b0111:  begin                     
+						ula_op <= 3'd0;
+						pc_en  <= 1'b1;
+						mem_wr <= 1'b0;
 					end
+		// DEFAULT
+		default:  begin                     
+						ula_op <= 3'dx;
+						pc_en  <= 1'bx;
+						mem_wr <= 1'bx;
+					end					
+
 	endcase
 end
 
